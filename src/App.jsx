a3cedd097c3e330064752a284700018142d3f98a
dev-rfgul <rfgul587@gmail.com>
2024-08-31 16:39:28 +0500

    added the reviews component and its fully working

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,18 @@ import Projects from './Component/Projects';
 import Certificates from './Component/Certificates';
 import Blog from './Component/Blog';
 import Contact from './Component/Contact';
-import Skills  from "./Component/Skills";
-
+import Skills from "./Component/Skills";
+import MarqueeDemo from './Component/Reviews';
 
 const App = () => {
   return (
     <div className="font-poppins mx-10">
+
       <Navbar />
+      <div className="App">
+        <h1 className="text-3xl font-bold text-center mb-8">Marquee Reviews</h1>
+        <MarqueeDemo />
+      </div>
       <Banner />
       <AboutMe />
       <Skills />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -20,6 +21,8 @@ module.exports = {
       animation: {
         marquee: "marquee var(--duration) linear infinite",
         "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       keyframes: {
         marquee: {
@@ -29,6 +32,14 @@ module.exports = {
         "marquee-vertical": {
           from: { transform: "translateY(0)" },
           to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       colors: {
@@ -70,20 +81,6 @@ module.exports = {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
