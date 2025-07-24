9b4fe5f11d3784f4295c546ccf885300be44b7a1
dev-rfgul <rfgul587@gmail.com>
2025-07-24 11:54:34 +0500

    writing splash screen so that user dont feel loading

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
         <Loader /> // Show the loader when loading
       ) : (
 
-        <div className="font-poppins ">
+        <div className="font-poppins">
           <Navbar />
           <Banner />
           <ScrollTimeline
